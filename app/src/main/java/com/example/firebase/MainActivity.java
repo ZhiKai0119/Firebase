cd60c0db45d0496279586926abe0fb1b4b78789c
ZhiKai0119 <chongzk-wm19@student.tarc.edu.my>
2020-07-22 11:26:38 +0800

    commit by zhikai

---
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -0,0 +1,141 @@
+package com.example.firebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "MainActivity";
+
+    private EditText email;
+    private EditText password;
+    private Button login;
+
+    public MainActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        email = (EditText) findViewById(R.id.email_id);
+        password = (EditText) findViewById(R.id.password_id);
+        login = (Button) findViewById(R.id.loginButton);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("message");
+
+        databaseReference.setValue("Another");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value = snapshot.getValue(String.class);
+                Toast.makeText(MainActivity.this, value, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    //user is signed in
+                    Log.d(TAG, "user signed in");
+                }else{
+                    //user is signed out
+                    Log.d(TAG, "user signed out");
+                }
+            }
+        };
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailString = email.getText().toString();
+                String pwd = password.getText().toString();
+
+                if (!emailString.equals("") && !pwd.equals("")) {
+                    mAuth.signInWithEmailAndPassword(emailString, pwd).addOnCompleteListener(MainActivity.this,
+                            new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(MainActivity.this, "Failed sign in", Toast.LENGTH_LONG).show();
+                            }else {
+                                Toast.makeText(MainActivity.this, "Signed in!!", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+//    public void LoginClicked(View view) {
+//        String emailString = email.getText().toString();
+//        String pwd = password.getText().toString();
+//
+//        if (!emailString.equals("") && !pwd.equals("")) {
+//            mAuth.signInWithEmailAndPassword(emailString, pwd).addOnCompleteListener(MainActivity.this,
+//                    new OnCompleteListener<AuthResult>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<AuthResult> task) {
+//
+//                            if (!task.isSuccessful()) {
+//                                Toast.makeText(MainActivity.this, "Failed sign in", Toast.LENGTH_LONG).show();
+//                            }else {
+//                                Toast.makeText(MainActivity.this, "Signed in!!", Toast.LENGTH_LONG).show();
+//                            }
+//                        }
+//                    });
+//        }
+//    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (mAuthListener != null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+}
